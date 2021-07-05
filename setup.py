7328155edc6a41f615f72364055711d42c8c06d8
noranhe <63988546+noranhe@users.noreply.github.com>
2021-07-05 15:05:59 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,66 @@
+import platform
+
+from setuptools import Extension, setup
+
+
+def get_ext_modules() -> list:
+    """
+    获取三方模块
+
+    Linux需要编译封装接口
+    Windows直接使用预编译的pyd即可
+    Mac由于缺乏二进制库支持无法使用
+    """
+    if platform.system() != "Linux":
+        return []
+
+    compiler_flags = [
+        "-std=c++17",
+        "-O3",
+        "-Wno-delete-incomplete", "-Wno-sign-compare",
+    ]
+    extra_link_args = ["-lstdc++"]
+    runtime_library_dirs = ["$ORIGIN"]
+
+    vnsoptmd = Extension(
+        "vnpy_sopt.api.vnsoptmd",
+        [
+            "vnpy_sopt/api/vnsopt/vnsoptmd/vnsoptmd.cpp",
+        ],
+        include_dirs=["vnpy_sopt/api/include",
+                      "vnpy_sopt/api/vnsopt"],
+        define_macros=[],
+        undef_macros=[],
+        library_dirs=["vnpy_sopt/api/libs", "vnpy_sopt/api"],
+        libraries=["soptthostmduserapi_se", "soptthosttraderapi_se"],
+        extra_compile_args=compiler_flags,
+        extra_link_args=extra_link_args,
+        runtime_library_dirs=runtime_library_dirs,
+        depends=[],
+        language="cpp",
+    )
+
+    vnsopttd = Extension(
+        "vnpy_sopt.api.vnsopttd",
+        [
+            "vnpy_sopt/api/vnsopt/vnsopttd/vnsopttd.cpp",
+        ],
+        include_dirs=["vnpy_sopt/api/include",
+                      "vnpy_sopt/api/vnsopt"],
+        define_macros=[],
+        undef_macros=[],
+        library_dirs=["vnpy_sopt/api/libs", "vnpy_sopt/api"],
+        libraries=["soptthostmduserapi_se", "soptthosttraderapi_se"],
+        extra_compile_args=compiler_flags,
+        extra_link_args=extra_link_args,
+        runtime_library_dirs=runtime_library_dirs,
+        depends=[],
+        language="cpp",
+    )
+
+    return [vnsopttd, vnsoptmd]
+
+
+setup(
+    ext_modules=get_ext_modules(),
+)
